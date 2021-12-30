347037a5c8bccab6f696b61e025e8d0dc1e3f33f
hceel <9224115@gmail.com>
2021-12-30 14:00:50 +0900

    *17-21. 댓글

--- src/app/controllers/postController.js
+++ src/app/controllers/postController.js
@@ -112,3 +112,106 @@ exports.deletePost = async function (req, res) {
         return res.send(isSuccess.false(500, `error: ${JSON.stringify(err)}`));
     }
 }; 
+
+//* 17. 댓글 작성
+exports.createComment = async function (req, res) {
+    const id = req.verifiedToken.id;
+    const postId = req.params.postId;
+    const content = req.body.content;
+    if (!id) return res.send(isSuccess.false(301, "토큰을 확인해주세요."));
+    
+    const createCommentQuery = `
+    insert into commentInfo(userId, content, postId)
+    values (?,?,?);
+        `;
+    try {
+        await pool.query(createCommentQuery, [id, content, postId]);
+        res.send(isSuccess.true(200, "입력성공"));
+    } catch (err) {
+        logger.error(`Query error\n: ${JSON.stringify(err)}`);
+        return res.send(isSuccess.false(500, `error: ${JSON.stringify(err)}`));
+    }
+}; 
+
+//* 18. 댓글 수정
+exports.updateComment = async function (req, res) {
+    const id = req.verifiedToken.id;
+    const commentId = req.params.commentId;
+    const content = req.body.content;
+    if (!id) return res.send(isSuccess.false(301, "토큰을 확인해주세요."));
+    
+    const updateCommentQuery = `
+    update commentInfo set content=? where idx=?;
+        `;
+    try {
+        await pool.query(updateCommentQuery, [content, commentId]);
+        res.send(isSuccess.true(200, "입력성공"));
+    } catch (err) {
+        logger.error(`Query error\n: ${JSON.stringify(err)}`);
+        return res.send(isSuccess.false(500, `error: ${JSON.stringify(err)}`));
+    }
+}; 
+
+//* 19. 댓글 삭제
+exports.deleteComment = async function (req, res) {
+    const id = req.verifiedToken.id;
+    const commentId = req.params.commentId;
+    if (!id) return res.send(isSuccess.false(301, "토큰을 확인해주세요."));
+    
+    const deleteCommentQuery = `
+    update commentInfo set status='DELETED' where idx=?;
+        `;
+    try {
+        await pool.query(deleteCommentQuery, [commentId]);
+        res.send(isSuccess.true(200, "입력성공"));
+    } catch (err) {
+        logger.error(`Query error\n: ${JSON.stringify(err)}`);
+        return res.send(isSuccess.false(500, `error: ${JSON.stringify(err)}`));
+    }
+}; 
+
+//* 20. 대댓글 작성
+exports.createRecomment = async function (req, res) {
+    const id = req.verifiedToken.id;
+    const postId = req.params.postId;
+    const commentId = req.params.commentId;
+    const content = req.body.content;
+    if (!id) return res.send(isSuccess.false(301, "토큰을 확인해주세요."));
+    
+    const createRecommentQuery = `
+    insert into commentInfo(userId, postId, parent, content)
+    values (?,?, ?,?);
+        `;
+    try {
+        await pool.query(createRecommentQuery, [id, postId, commentId, content]);
+        res.send(isSuccess.true(200, "입력성공"));
+    } catch (err) {
+        logger.error(`Query error\n: ${JSON.stringify(err)}`);
+        return res.send(isSuccess.false(500, `error: ${JSON.stringify(err)}`));
+    }
+}; 
+
+//*21. 댓글창 조회
+exports.getCommentList = async function (req, res) {    
+    const id = req.verifiedToken.id;
+    const postId = req.params.postId;
+    if (!id) return res.send(isSuccess.false(301, "토큰을 확인해주세요."));
+
+    const getCommentListQuery = `select idx, userId, content,
+    case
+        when TIMESTAMPDIFF(HOUR, createdAt, CURRENT_TIMESTAMP) < 24
+            then DATE_FORMAT(createdAt, '%H:%i')
+        else DATE_FORMAT(createdAt, '%Y.%m.%d')
+    end as postCreateTime
+    from commentInfo
+    where postId=? and status='ACTIVE'
+    order by if(isnull(parent), idx, parent);`;
+
+    try {
+        const [getCommentListRows] = await pool.query(getCommentListQuery,[postId]);
+        res.send(isSuccess.true(200, "조회성공(입력값 ㅇ)", getCommentListRows));
+    } catch (err) {
+        logger.error(`Query error\n: ${JSON.stringify(err)}`);
+        return res.send(isSuccess.false(500, `error: ${JSON.stringify(err)}`));
+    }
+};//댓글 테이블을 하나로 관리했는데 이러면 클라이언트에서는 어떻게 사용할지 모르겠어서... 추후 수정
--- src/app/routes/postRoute.js
+++ src/app/routes/postRoute.js
@@ -8,4 +8,11 @@ module.exports = function(app){
     app.get('/post/:postId', jwtMiddleware, post.getPostInfo);                      //* 14. 게시글 상세조회
     app.route('/post/:postId').patch(jwtMiddleware, post.updatePost);               //* 15. 게시글 수정
     app.route('/post/:postId').delete(jwtMiddleware, post.deletePost);              //* 16. 게시글 삭제
+
+    app.route('/:postId/comment').post(jwtMiddleware, post.createComment);          //* 17. 댓글 작성
+    app.route('/comment/:commentId').patch(jwtMiddleware, post.updateComment);      //* 18. 댓글 수정
+    app.route('/comment/:commentId').delete(jwtMiddleware, post.deleteComment);     //* 19. 댓글 삭제
+    app.route('/:postId/:commentId/recomment').post(jwtMiddleware, post.createRecomment);   //* 20. 대댓글 작성
+    app.get('/:postId/comment', jwtMiddleware, post.getCommentList);                //*21. 댓글창 조회
+
 };
